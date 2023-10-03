7ddbfa4b5aefc7fbc419aab9bdf5215f7d2a42ae
Gunveen Kaur <sainiaryan2222@gmail.com>
2023-10-03 23:34:01 +0530

    any length of word

--- app/page.js
+++ app/page.js
@@ -38,7 +38,7 @@ export default function Home() {
 
   async function fetchWord() {
     const response = await fetch(
-      'https://random-word-api.herokuapp.com/word?length=5'
+      'https://random-word-api.herokuapp.com/word?number=10'
     );
     let randomWord = await response.json();
     setSuccess(false);
