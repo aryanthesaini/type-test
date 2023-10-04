d1171d9b7b69a49ff18f3695a7fd88157aa16e59
Gunveen Kaur <sainiaryan2222@gmail.com>
2023-10-04 20:58:17 +0530

    added levels

--- app/page.js
+++ app/page.js
@@ -4,37 +4,46 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 import { TagCloud } from '@frank-mayer/react-tag-cloud';
 let i = 0;
+let level = 1;
+let numberOfWords = 2;
 export default function Home() {
   const [word, setWord] = useState(null);
 
-  const [success, setSuccess] = useState('');
   const [correctPresses, setCorrectPresses] = useState(0);
   const [incorrectPresses, setIncorrectPresses] = useState(0);
   const [curWord, setCurWord] = useState('');
   const [accuracy, setAccuracy] = useState(0.0);
+  const [pressedKey, setPressedKey] = useState('');
+  const [validClass, setValidClass] = useState('text-blue-600');
 
   useEffect(() => {
     const keyDownHandler = (e) => {
       const key = e.key.toLowerCase();
 
       if (key >= 'a' && key <= 'z') {
-        // console.log(curWord);
+        setPressedKey(key);
         let checkAlpha = curWord[0];
 
         if (curWord.length > 0 && key == checkAlpha) {
           setCorrectPresses(correctPresses + 1);
 
           word[i] = curWord.slice(1, curWord.length);
+          setValidClass('text-green-500');
           if (curWord.length == 1 && key == checkAlpha) {
             i++;
-            if (i == 20) {
+            setValidClass('text-blue-600');
+            if (i == numberOfWords) {
+              level++;
+              numberOfWords += 3;
               fetchWord();
               i = 0;
             }
           }
+
           setCurWord(word[i]);
         } else if (curWord.length > 0 && key != checkAlpha) {
           setIncorrectPresses(incorrectPresses + 1);
+          setValidClass('text-red-500');
         }
 
         setAccuracy(
@@ -55,7 +64,7 @@ export default function Home() {
 
   async function fetchWord() {
     const response = await fetch(
-      'https://random-word-api.herokuapp.com/word?number=20'
+      `https://random-word-api.herokuapp.com/word?number=${numberOfWords}`
     );
     let randomWord = await response.json();
 
@@ -66,11 +75,13 @@ export default function Home() {
   const options = {
     maxSpeed: 'fast',
   };
+
   return (
     <>
       {' '}
       {word && (
         <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
+          <h1 className='text-2xl uppercase'>Level {level}</h1>
           <TagCloud
             options={options}
             className='text-sm uppercase text-gray-500'
@@ -79,22 +90,38 @@ export default function Home() {
           </TagCloud>
 
           <div className='flex items-center justify-center space-x-7'>
-            <h1 className='text-5xl font-bold uppercase text-blue-600'>
+            {/* <h1 className='text-5xl font-bold uppercase text-blue-600'>
+              {curWord.split('').map((character) => (
+                <div key={Math.random()} className={`inline ${validClass}`}>
+                  {character}
+                </div>
+              ))}
+            </h1> */}
+
+            <h1 className={`text-5xl font-bold uppercase  ${validClass}`}>
               {curWord}
             </h1>
           </div>
-          <div className='flex items-center justify-center space-x-7'>
-            <p>Correct keys pressed</p>
-            <h1 className='text-2xl uppercase text-green-500'>
-              {correctPresses}
-            </h1>
-          </div>
-          <div className='flex items-center justify-center space-x-7'>
-            <p>Incorrect keys pressed</p>
 
-            <h1 className='text-2xl uppercase text-red-500'>
-              {incorrectPresses}
-            </h1>
+          {/* <div className='flex items-center justify-center space-x-7'>
+            <h2>
+              You pressed:{' '}
+              <span className='text-xl font-semibold uppercase text-blue-400'>
+                {pressedKey}
+              </span>
+            </h2>
+          </div> */}
+
+          <div className='flex items-center justify-center space-x-10'>
+            <div className='px-10 py-10'>
+              <p>Correct keys pressed</p>
+              <h1 className=' uppercase text-green-500'>{correctPresses}</h1>
+            </div>
+            <div className='px-10 py-10'>
+              <p>Incorrect keys pressed</p>
+
+              <h1 className=' uppercase text-red-500'>{incorrectPresses}</h1>
+            </div>
           </div>
 
           <div className='flex items-center justify-center space-x-7'>
