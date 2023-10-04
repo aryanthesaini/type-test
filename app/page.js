4bfd345fe1181e43b61fab1bb244ece39f0024b1
Gunveen Kaur <sainiaryan2222@gmail.com>
2023-10-04 16:41:47 +0530

    20 pre loaded words

--- app/page.js
+++ app/page.js
@@ -3,30 +3,43 @@
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { TagCloud } from '@frank-mayer/react-tag-cloud';
+let i = 0;
 export default function Home() {
-  const [word, setWord] = useState('water');
+  const [word, setWord] = useState(null);
 
   const [success, setSuccess] = useState('');
   const [correctPresses, setCorrectPresses] = useState(0);
   const [incorrectPresses, setIncorrectPresses] = useState(0);
+  const [curWord, setCurWord] = useState('');
+  const [accuracy, setAccuracy] = useState(0.0);
 
   useEffect(() => {
     const keyDownHandler = (e) => {
       const key = e.key.toLowerCase();
+
       if (key >= 'a' && key <= 'z') {
-        let checkAlpha = word[0];
-        if (word.length == 1 && key == checkAlpha) {
+        // console.log(curWord);
+        let checkAlpha = curWord[0];
+
+        if (curWord.length > 0 && key == checkAlpha) {
           setCorrectPresses(correctPresses + 1);
-          setWord('');
-          setSuccess('Good');
-          fetchWord();
-        } else if (word.length > 0 && key == checkAlpha) {
-          setCorrectPresses(correctPresses + 1);
-          let curWord = word.slice(1, word.length);
-          setWord(curWord);
-        } else {
+
+          word[i] = curWord.slice(1, curWord.length);
+          if (curWord.length == 1 && key == checkAlpha) {
+            i++;
+            if (i == 20) {
+              fetchWord();
+              i = 0;
+            }
+          }
+          setCurWord(word[i]);
+        } else if (curWord.length > 0 && key != checkAlpha) {
           setIncorrectPresses(incorrectPresses + 1);
         }
+
+        setAccuracy(
+          (correctPresses / (correctPresses + incorrectPresses)) * 100
+        );
       }
     };
     document.addEventListener('keydown', keyDownHandler);
@@ -36,29 +49,63 @@ export default function Home() {
     };
   });
 
+  useEffect(() => {
+    fetchWord();
+  }, []);
+
   async function fetchWord() {
     const response = await fetch(
-      'https://random-word-api.herokuapp.com/word?number=10'
+      'https://random-word-api.herokuapp.com/word?number=20'
     );
     let randomWord = await response.json();
-    setSuccess(false);
-    setWord(randomWord[0]);
+
+    setWord(randomWord);
+    setCurWord(randomWord[0]);
   }
 
   const options = {
-    maxSpeed: 'fast',
+    maxSpeed: 'medium',
   };
   return (
-    <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
-      <TagCloud
-        options={options}
-        className='text-5xl uppercase text-white'
-        onClickOptions={{ passive: true }}>
-        {[word]}
-      </TagCloud>
+    <>
+      {' '}
+      {word && (
+        <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
+          <TagCloud
+            options={options}
+            className='text-sm uppercase text-gray-500'
+            onClickOptions={{ passive: true }}>
+            {word}
+          </TagCloud>
 
-      <h1 className='text-2xl uppercase text-green-500'>{correctPresses}</h1>
-      <h1 className='text-2xl uppercase text-red-500'>{incorrectPresses}</h1>
-    </div>
+          <div className='flex items-center justify-center space-x-7'>
+            <h1 className='text-5xl font-bold uppercase text-blue-600'>
+              {curWord}
+            </h1>
+          </div>
+          <div className='flex items-center justify-center space-x-7'>
+            <p>Correct keys pressed</p>
+            <h1 className='text-2xl uppercase text-green-500'>
+              {correctPresses}
+            </h1>
+          </div>
+          <div className='flex items-center justify-center space-x-7'>
+            <p>Incorrect keys pressed</p>
+
+            <h1 className='text-2xl uppercase text-red-500'>
+              {incorrectPresses}
+            </h1>
+          </div>
+
+          <div className='flex items-center justify-center space-x-7'>
+            <p>Accuracy: </p>
+
+            <h1 className='text-2xl uppercase text-gray-300'>
+              {accuracy.toFixed(2)}%
+            </h1>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
